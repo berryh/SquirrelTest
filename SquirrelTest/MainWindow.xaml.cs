893a8578dfb6bfcf32a35a18ba11517fd3b6d8b1
berryh <berryh@aol.nl>
2015-12-28 16:33:30 +0100

    Initial Commit

---
+++ SquirrelTest/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Squirrel;
+
+namespace SquirrelTest
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Task.Run(async () =>
+            {
+                using (var mgr = new UpdateManager(@"E:\Documenten\Visual Studio 2015\Projects\SquirrelTest\Releases"))
+                {
+                    await mgr.UpdateApp();
+                }
+            });
+        }
+    }
+}
